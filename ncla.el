01460e90dfe096db2e4f6e0f61387d0901b3d0fd
Linas Vidziunas <linasvidz@gmail.com>
2021-12-25 23:40:45 +0100

    get-applications func filters away terminal apps and retruns comment

--- ncla.el
+++ ncla.el
@@ -11,21 +11,22 @@
     (setq preprocessed-cmd (assoc (completing-read "Start application: "
 						   desktop-files nil t)
 				  desktop-files))
-
     (setq name (car preprocessed-cmd))
 
-    (setq preprocessed-cmd (split-string (cdr preprocessed-cmd) " " t))
+
+    (setq preprocessed-cmd (split-string (car (cdr preprocessed-cmd)) " " t))
 
     (dolist (element preprocessed-cmd)
       (if (not (string-match-p "%" element))
 	  (setq cmd (cons cmd element)))) 
     (start-process-shell-command name nil (cdr cmd))))
 
+
 (defun ncla--get-applications (desktop-file-paths)
   "Returns list with a list of application name and command"
   (let (applications)
     (dolist (file-path desktop-file-paths)
-      (let (current-application nameb execb)
+      (let (name exec comment terminal)
 	(catch 'done
 	  (dolist (line (split-string 
 			 (with-temp-buffer
@@ -33,17 +34,34 @@
 			   (buffer-substring-no-properties
 			    (point-min)
 			    (point-max))) "\n" t))
+
 	    (when (>= (length line) 5)
-	      (when (string-match-p "Name=" (substring line 0 5))
-		(setq current-application (substring line 5 (length line)))
-		(setq nameb 't))
-	      (when (string-match-p "Exec=" (substring line 0 5))
-		(setq current-application
-		      (cons current-application (substring line 5 (length line)) ))
-		(setq execb 't))
-	      (when (and nameb execb)
-		(throw 'done current-application)))))
-	(setq applications (cons current-application applications))))
+
+	      (when (and (string-match-p "Name=" (substring line 0 5))
+			 (not name))
+		(setq name (substring line 5 (length line))))
+	      
+	      (when (and (string-match-p "Exec=" (substring line 0 5))
+			 (not exec))
+		(setq exec (substring line 5 (length line))))
+		
+	      (when (>= (length line) 9)
+		(when (and (string-match-p "Terminal=" (substring line 0 9))
+			   (not terminal))
+		  (setq terminal (substring line 9 (length line))))
+		
+		(when (and (string-match-p "Comment=" (substring line 0 8))
+			   (not comment))
+		  (setq comment (substring line 8 (length line)))))
+	      
+	      ;; (when comment terminal name exec
+	      ;; 	    (throw 'done t))))
+	      ))
+	
+	(when (or (not terminal) (string-match-p terminal "false"))
+	  (when (and name exec)
+	    (setq applications
+	     (cons (list name exec comment) applications)))))))
     
     ;; Black magic to return applications
     (push (pop applications) applications)))
